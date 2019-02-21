4d18e9a5c213b482a6b01a9d66be5097ac168344
davidferro <david-ferro@sbcglobal.net>
2019-02-20 21:24:29 -0500

    deleted some files

--- keys.js
+++
@@ -1,7 +0,0 @@
-exports.MySQL = {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    DIALECT: process.env.DIALECT
-};
